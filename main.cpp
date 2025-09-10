bc56fbb7d93e1ff8b12ace92708fba0165e689f0
bughunter-mano <akbarmano12345@gmail.com>
2025-09-10 10:51:48 +0500

    changes to main file

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
+    cout<<"hiii";
     cout << "Hello, my name is Malaika!" << endl;
+    int age;
+cout << "Enter your age: ";
+cin >> age;
+if (age >= 18) cout << "You are eligible to vote.";
+else cout << "Not eligible.";
+
     return 0;
 }
